dd0182e04e7c20db754c6c391c19a315d5902932
Ricardo Vera Nervi <rveranervi@gmail.com>
2019-06-01 14:31:31 -0500

    feat(logininicio) meu inicio

--- app/src/main/java/pe/alertteam/alertaciudadano/InicioActivity.java
+++ app/src/main/java/pe/alertteam/alertaciudadano/InicioActivity.java
@@ -1,33 +1,79 @@
 package pe.alertteam.alertaciudadano;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class InicioActivity extends AppCompatActivity {
 
-    Button btnLogout;
+    private DrawerLayout mDrawerLayout;
+    private pe.alertteam.alertaciudadano.Utils.Menu menu;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
-        btnLogout = findViewById(R.id.button);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        menu = new pe.alertteam.alertaciudadano.Utils.Menu(navigationView);
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent i = new Intent(InicioActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
-            }
-        });
+        setSupportActionBar(toolbar);
+
+        cargarMenu();
+    }
+
+    private void cargarMenu(){
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            VectorDrawableCompat indicator = VectorDrawableCompat.create(getResources(), R.drawable.ic_menu_black_24dp, getTheme());
+            indicator.setTint(ResourcesCompat.getColor(getResources(),R.color.colorWhite,getTheme()));
+            supportActionBar.setHomeAsUpIndicator(indicator);
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        // TODO: handle navigation
+                        menu.seleccionarOpcionMainMenu(InicioActivity.this, menuItem);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.menu_conf, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/pe/alertteam/alertaciudadano/Utils/Menu.java
@@ -0,0 +1,112 @@
+package pe.alertteam.alertaciudadano.Utils;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import pe.alertteam.alertaciudadano.InicioActivity;
+import pe.alertteam.alertaciudadano.MainActivity;
+import pe.alertteam.alertaciudadano.R;
+
+public class Menu {
+
+    private FirebaseUser usuario;
+    private FirebaseFirestore db;
+    private static final String TAG = "Menu";
+
+    public Menu(final NavigationView navigationView) {
+        usuario = FirebaseAuth.getInstance().getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        db.collection("users").whereEqualTo("usuario", usuario.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                final Map<String, Object> info = document.getData();
+                                final TextView tvTitulo = navigationView.getHeaderView(0).findViewById(R.id.txt_titulo);
+                                TextView tvSub = navigationView.getHeaderView(0).findViewById(R.id.txt_subtitulo);
+                                String nombrecompleto = info.get("nombres").toString().toUpperCase().split(" ")[0] + " " + info.get("apellidos").toString().toUpperCase().split(" ")[0];
+                                if(nombrecompleto.trim().length()>20){
+                                    tvTitulo.setText(nombrecompleto.substring(20));
+                                }
+                                else{
+                                    tvTitulo.setText(nombrecompleto);
+                                }
+                                if(usuario.getPhotoUrl() != null){
+                                    Picasso.with(navigationView.getContext()).load(usuario.getPhotoUrl()).into(new Target() {
+                                        @Override
+                                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                                            Drawable drawableImage = new BitmapDrawable(navigationView.getResources(), bitmap);
+                                            tvTitulo.setCompoundDrawablesWithIntrinsicBounds(drawableImage, null, null, null);
+                                        }
+
+                                        @Override
+                                        public void onBitmapFailed(Drawable errorDrawable) { }
+
+                                        @Override
+                                        public void onPrepareLoad(Drawable placeHolderDrawable) {}
+                                    });
+                                }
+                                tvSub.setText(info.get("type").toString().trim() + " - " + usuario.getEmail());
+                                navigationView.getMenu().clear();
+                                navigationView.inflateMenu(R.menu.menu_navigation);
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void seleccionarOpcionMainMenu(Activity activity, MenuItem menuItem) {
+        Intent i;
+        switch (menuItem.getItemId()){
+            case R.id.inicio:
+                i = new Intent(activity, InicioActivity.class);
+                activity.startActivity(i);
+                break;
+            case R.id.reportes:
+
+                break;
+            case R.id.grupos:
+
+                break;
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                i = new Intent(activity, MainActivity.class);
+                activity.startActivity(i);
+                break;
+            default:
+                break;
+        }
+        activity.finish();
+    }
+}
