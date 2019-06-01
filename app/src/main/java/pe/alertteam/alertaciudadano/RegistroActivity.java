90597fd0917c1cde4851faea94d6505ab192ee3f
Ricardo Vera Nervi <rveranervi@gmail.com>
2019-06-01 12:13:35 -0500

    feat(login)Inicio de sesion usuario y clave

--- app/src/main/java/pe/alertteam/alertaciudadano/RegistroActivity.java
+++ app/src/main/java/pe/alertteam/alertaciudadano/RegistroActivity.java
@@ -1,14 +1,59 @@
 package pe.alertteam.alertaciudadano;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    EditText edtCorreo;
+    EditText edtPassword;
+    EditText edtNombres;
+    EditText edtApellidos;
+    EditText edtNumDocumento;
+    EditText edtUbigeo;
+    EditText edtDireccion;
+    Button btnRegistrar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.createUserWithEmailAndPassword(edtCorreo.getText().toString().trim(), edtPassword.getText().toString().trim())
+                        .addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                } else {
+                                    Log.w("Regitro", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(RegistroActivity.this, "No se registro.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
