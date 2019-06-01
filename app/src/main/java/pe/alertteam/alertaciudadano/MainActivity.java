0ffbbaad6a676e689e8178fff0b5deb20888dc23
Ricardo Vera Nervi <rveranervi@gmail.com>
2019-06-01 11:20:35 -0500

    feat(app)Init

---
+++ app/src/main/java/pe/alertteam/alertaciudadano/MainActivity.java
@@ -0,0 +1,68 @@
+package pe.alertteam.alertaciudadano;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtCorreo;
+    EditText edtPassword;
+    Button btnLogin;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        edtCorreo = (EditText) findViewById(R.id.editText);
+        edtPassword = (EditText) findViewById(R.id.editText2);
+        btnLogin = (Button) findViewById(R.id.button);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signInWithEmailAndPassword(edtCorreo.getText().toString().trim(), edtPassword.getText().toString().trim())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Intent i = new Intent(MainActivity.this, InicioActivity.class);
+                                    startActivity(i);
+                                    finish();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "No se pudo inciar sesión.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent i = new Intent(this, InicioActivity.class);
+            startActivity(i);
+            finish();
+        }
+    }
+}
