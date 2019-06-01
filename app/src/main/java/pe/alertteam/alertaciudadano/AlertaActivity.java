a29c2f1793972bb0c1930dd59d25050465528639
JP <jpbarriga.1989@gmail.com>
2019-06-01 17:58:05 -0500

    feat(alerta) subir imagen

--- app/src/main/java/pe/alertteam/alertaciudadano/AlertaActivity.java
+++ app/src/main/java/pe/alertteam/alertaciudadano/AlertaActivity.java
@@ -1,9 +1,13 @@
 package pe.alertteam.alertaciudadano;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,6 +30,10 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.ServerTimestamp;
 import com.google.firebase.firestore.model.value.ServerTimestampValue;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.firestore.v1beta1.DocumentTransform;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
@@ -34,13 +42,27 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+
 public class AlertaActivity extends AppCompatActivity {
     EditText txtTitulo;
     EditText txtDescripcion;
     //EditText txtUbigeo;
     Button btnAlertar;
+    private Button btnChoose;
+
+    private Uri filePath;
+
+    private final int PICK_IMAGE_REQUEST = 71;
     private FirebaseAuth mAuth;
     FirebaseFirestore db;
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +73,14 @@ public class AlertaActivity extends AppCompatActivity {
         txtDescripcion = (EditText) findViewById(R.id.txtDescripcion);
         //txtUbigeo = (EditText) findViewById(R.id.txtUbigeo);
         btnAlertar = (Button) findViewById(R.id.btnAlertar);
+        btnChoose = (Button) findViewById(R.id.btnChoose);
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
         btnAlertar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -89,6 +119,8 @@ public class AlertaActivity extends AppCompatActivity {
                 perfil.put("uidMunicipalidad", "333333");
                 perfil.put("nombreUsuario", "test");
                 perfil.put("titulo", txtTitulo.getText().toString().trim());
+
+                uploadImage();
                 db.collection("alerta" +
                         "")
                         .add(perfil)
@@ -106,45 +138,50 @@ public class AlertaActivity extends AppCompatActivity {
                                 Log.w("Regitro", "Error adding document", e);
                             }
                         });
-//                mAuth.createUserWithEmailAndPassword(edtCorreo.getText().toString().trim(), edtPassword.getText().toString().trim())
-//                        .addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
-//                            @Override
-//                            public void onComplete(@NonNull Task<AuthResult> task) {
-//                                if (task.isSuccessful()) {
-//                                    FirebaseUser user = mAuth.getCurrentUser();
-//                                    Map<String, Object> perfil = new HashMap<>();
-//                                    perfil.put("usuario", user.getUid());
-//                                    perfil.put("nombres", edtNombres.getText().toString().trim());
-//                                    perfil.put("apellidos", edtApellidos.getText().toString().trim());
-//                                    perfil.put("dni", edtNumDocumento.getText().toString().trim());
-//                                    perfil.put("ubigeo", edtUbigeo.getText().toString().trim());
-//                                    perfil.put("direccion", edtDireccion.getText().toString().trim());
-//
-//                                    db.collection("users")
-//                                            .add(user)
-//                                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                                                @Override
-//                                                public void onSuccess(DocumentReference documentReference) {
-//                                                    Toast.makeText(AlertaActivity.this, "Se creo el usuario correctaente.", Toast.LENGTH_SHORT).show();
-//                                                    Log.d("Regitro", "DocumentSnapshot added with ID: " + documentReference.getId());
-//                                                    finish();
-//                                                }
-//                                            })
-//                                            .addOnFailureListener(new OnFailureListener() {
-//                                                @Override
-//                                                public void onFailure(@NonNull Exception e) {
-//                                                    Log.w("Regitro", "Error adding document", e);
-//                                                }
-//                                            });
-//
-//                                } else {
-//                                    Log.w("Regitro", "createUserWithEmail:failure", task.getException());
-//                                    Toast.makeText(AlertaActivity.this, "No se registro.",
-//                                            Toast.LENGTH_SHORT).show();
-//                                }
-//                            }
-//                        });
+
             }
         });
     }
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+    private void uploadImage() {
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("storage_reportes/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(AlertaActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(AlertaActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
+
 }
