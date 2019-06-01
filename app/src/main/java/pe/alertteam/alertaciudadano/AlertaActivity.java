eb37c41e4e1abd3ccb4f8f3dcaa3ae6a0883d75a
JP <jpbarriga.1989@gmail.com>
2019-06-01 13:59:16 -0500

    feat(gitignore) files added

---
+++ app/src/main/java/pe/alertteam/alertaciudadano/AlertaActivity.java
@@ -0,0 +1,14 @@
+package pe.alertteam.alertaciudadano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlertaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alerta);
+    }
+}
