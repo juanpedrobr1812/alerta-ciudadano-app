bb3e2b30cdaba9d415112ab9cacea0eda9194b63
Ricardo Vera Nervi <rveranervi@gmail.com>
2019-06-01 11:55:40 -0500

    feat(login)Inicio de sesion usuario y clave

--- app/src/main/java/pe/alertteam/alertaciudadano/InicioActivity.java
+++ app/src/main/java/pe/alertteam/alertaciudadano/InicioActivity.java
@@ -2,13 +2,32 @@ package pe.alertteam.alertaciudadano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class InicioActivity extends AppCompatActivity {
+
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        btnLogout = findViewById(R.id.button);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(InicioActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
